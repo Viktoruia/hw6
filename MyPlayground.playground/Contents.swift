d6ca0db6cdfcfd822f67c001d5db33956e52e3e9
Виктория <victoriaklisova@gmail.com>
2025-07-17 00:25:29 +0300

    add task2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -16,3 +16,21 @@ let newNumbersArray = Array(unionNumbersArrays)
 let sortedArray = newNumbersArray.sorted { $0 < $1 }
 print("Первый массив чисел - \(numbersArray1). Второй массив чисел - \(numbersArray2). Новый массив чисел, объединяющий их - \(sortedArray)")
 
+
+//2. Создать функцию, которая принимает массив int. Возвести все Int в квадрат.
+//Возвратить новый массив.
+
+// 1 способ
+let intArray = [1, 2, 3, 4, 5, 6, 7]
+let newIntArray = intArray.map { $0 * $0 }
+print("Массив - \(intArray) и массив с его квадратами - \(newIntArray)")
+
+// 2 способ
+func intArray2(_ array: [Int]) -> [Int] {
+    var newIntArray2: [Int] = []
+    for element in array {
+        newIntArray2.append(element * element)
+    }
+    return newIntArray2
+}
+print(intArray2([2, 4, 6, 8]))
